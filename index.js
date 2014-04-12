4a1a9fe0e5f0d7348d94f3380be19119ccd77240
Josh Taylor <joshrtay@gmail.com>
2014-04-11 23:43:36 -0700

    fix ssh stream

--- index.js
+++ index.js
@@ -1,52 +1,60 @@
 var ssh2 = require('ssh2');
 var consoleStream = require('console-stream');
 var fs = require('fs');
+var path = require('path');
 var through = require('through');
 var es = require('event-stream');
-var conn = new ssh2();
+
+
 module.exports = function(config) {
-
+  var conn = new ssh2();
   var ready = false;
-  return es.through(function(data) {
+  var commandStream = es.through();
+  commandStream.pause();
+
+  var outStream = es.through(function(data) {
+    commandStream.queue(data);
+  }, function(){
+    commandStream.queue(null);
+  });
+
+  commandStream.pipe(es.through(function(cmd) {
     var self = this;
-    if(! ready) {
-      console.log('not ready');
-      this.pause();
-      conn.connect(config);
-      conn.on('ready', function() {
-        self.resume();
-        self.emit('data', data);
-        ready = true;
-        console.log('ready');
+    this.pause();
+    console.log('command', cmd);
+    conn.exec(cmd, function(err, stream) {
+      stream.on('data', function(data) {
+        outStream.emit('data', data)
       });
-    } else
-      self.emit('data', data);
-  })
-};
+      stream.on('end', function() {
+        self.resume();
+      }); 
+    });
+  }, function(){
+    outStream.emit('end');
+    conn.end();
+  }))
 
-function exec() {
+  conn.connect(config);
+  conn.on('ready', function() {
+    console.log('ready')
+    ready = true;
+    commandStream.resume();
+  });
 
+  return outStream;
 }
 
+
+
+/* Example:
 fs.createReadStream('./test.sh')
   .pipe(es.split())
   .pipe(module.exports({
   host: '107.170.198.91',
   username: 'task',
-  privateKey: fs.readFileSync('/Users/andrewshaffer/.ssh/id_rsa')
+  privateKey: fs.readFileSync(path.resolve(process.env.HOME, '.ssh/id_rsa'))
 }))
-  .pipe(es.through(function(data) {
-    this.pause();
-    console.log('commmand', data);
-    var self = this;
-    conn.exec(data, function(err, stream) {
-      stream.on('data', function(data) {
-        self.emit('data', data);
-      })
-      .on('end', function() {
-        console.log('resume');
-        self.resume();
-      });
-    })
-  }))
   .pipe(consoleStream());
+
+*/
--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-sleep 5
+sleep 2
 ls -a
